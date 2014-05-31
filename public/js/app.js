4204a81d8ad5511b9b20b20a385bba0521509068
pussinboots <pussinboots666@googlemail.com>
2014-05-31 18:28:07 +0200

    - simple table based design for the overview page

--- public/js/app.js
+++ public/js/app.js
@@ -1,11 +1,12 @@
 'use strict';
 
 /* App Module */
-var demoApp = angular.module('demoApp', ['services']);
+var demoApp = angular.module('demoApp', ['services', 'DataTable']);
 
 demoApp.config(function ($routeProvider) {
     $routeProvider
-        .when('/travis', { templateUrl: 'partials/travis.html', controller: TravisController})
+        .when('/travis', { templateUrl: 'partials/overview.html', controller: TravisController})
+        .when('/details', { templateUrl: 'partials/details.html', controller: TravisController})
         .when('/projects', { templateUrl: 'partials/projects.html', controller: ProjectController})
 	.when('/new', { templateUrl: 'partials/new-project.html', controller: ProjectController})
         .otherwise({ redirectTo: '/travis' });
